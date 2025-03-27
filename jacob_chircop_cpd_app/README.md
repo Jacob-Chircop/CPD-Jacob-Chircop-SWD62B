03305f4eb1270f5efe7fbe130b59fad48a26216c
Jacob-Chircop <jacob.chircop.g53945@mcast.edu.mt>
2025-03-27 14:17:09 +0100

    Created Empty Flutter App

---
+++ jacob_chircop_cpd_app/README.md
@@ -0,0 +1,3 @@
+# jacob_chircop_cpd_app
+
+A new Flutter project.
---
+++ jacob_chircop_cpd_app/android/app/src/main/kotlin/com/example/jacob_chircop_cpd_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jacob_chircop_cpd_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
